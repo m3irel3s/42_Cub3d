859c84d322c39b35188fe1a6f81b50c6305ee419
Melanie Reis <melaniereis@Mac.Home>
2025-04-01 21:47:15 +0100

    add cleanup after destroying windows, image and display

--- src/900_cleanup/free.c
+++ src/900_cleanup/free.c
@@ -3,24 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:35:42 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/04/01 12:12:36 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/04/01 21:44:48 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 static void	ft_clean_map(t_map * map);
+static void	ft_clean_graphics(t_game *game);
 
 void	ft_cleanup(t_game *game, char *msg, int fd)
 {
-	ft_clean_map(game->map);
-	ft_free(game->map);
-	ft_free(game);
-	ft_printf(fd, msg);
-	exit(0);
+	if (game)
+	{
+		ft_clean_map(game->map);
+		ft_clean_graphics(game);
+		ft_free(game->map);
+		ft_free(game->player);
+		ft_free(game);
+	}
+	ft_printf(fd, "%s\n", msg);
+	exit(EXIT_SUCCESS);
 }
 
 static void	ft_clean_map(t_map * map)
@@ -29,3 +35,18 @@ static void	ft_clean_map(t_map * map)
 	ft_free(map->paths);
 }
 
+static void	ft_clean_graphics(t_game *game)
+{
+	if (!game || !game->mlx)
+		return;
+	if (game->img)
+	{
+		if (game->img->mlx_img)
+			mlx_destroy_image(game->mlx, game->img->mlx_img);
+		ft_free(game->img);
+	}
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
+	ft_free(game->mlx);
+}
