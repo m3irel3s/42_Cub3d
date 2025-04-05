4fc305b2cc1f35e21837c506827e6b99fcba4195
Melanie Ferraz Reis <meferraz@c1r2s6.42porto.com>
2025-04-05 09:38:18 +0100

    fix segfault issue in dda algorithm

--- src/300_raycasting/dda_raycasting.c
+++ src/300_raycasting/dda_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 22:13:53 by meferraz          #+#    #+#             */
-/*   Updated: 2025/04/04 22:21:16 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/04/05 09:36:21 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,6 @@ void	ft_perform_dda(t_game *game, t_ray *ray)
 {
 	while (ray->hit == 0)
 	{
-		if (ray->map_x < 0 || ray->map_x >= (int)game->map->width ||
-			ray->map_y < 0 || ray->map_y >= (int)game->map->height)
-		{
-			ray->hit = 1;
-			ray->side = 0;
-			break ;
-		}
 		if (ray->step.side_dist_x < ray->step.side_dist_y)
 		{
 			ray->step.side_dist_x += ray->step.delta_dist_x;
@@ -34,6 +27,13 @@ void	ft_perform_dda(t_game *game, t_ray *ray)
 			ray->step.side_dist_y += ray->step.delta_dist_y;
 			ray->map_y += ray->step.step_y;
 			ray->side = 1;
+		}
+		if (ray->map_x < 0 || ray->map_x >= (int)game->map->width ||
+			ray->map_y < 0 || ray->map_y >= (int)game->map->height)
+		{
+			ray->hit = 1;
+			ray->side = 0;
+			break ;
 		}
 		if (game->map->grid[ray->map_y][ray->map_x] == '1')
 			ray->hit = 1;
