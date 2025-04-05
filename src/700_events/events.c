341f9024ee6f8e09fd668d41effdd61a58b4c8af
Melanie Ferraz Reis <meferraz@c1r2s6.42porto.com>
2025-04-05 13:49:30 +0100

    correct key handlers

--- src/700_events/events.c
+++ src/700_events/events.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:30:00 by meferraz          #+#    #+#             */
-/*   Updated: 2025/04/05 09:17:23 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/04/05 13:47:39 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ int	ft_handle_key(int key, t_game *game)
 		ft_move_forward(game, move_speed);
 	else if (key == XK_s)
 		ft_move_backward(game, move_speed);
-	else if (key == XK_d)
-		ft_move_left(game, move_speed);
 	else if (key == XK_a)
+		ft_move_left(game, move_speed);
+	else if (key == XK_d)
 		ft_move_right(game, move_speed);
 	else if (key == XK_Left)
 		ft_rotate_left(game, rot_speed);
