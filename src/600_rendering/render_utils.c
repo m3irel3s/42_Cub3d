f96d74469b7d4515ac1b9084e69b27b56727fa93
Melanie Reis <melaniereis@Mac.Home>
2025-04-10 17:36:08 +0100

    fix norminette

--- src/600_rendering/render_utils.c
+++ src/600_rendering/render_utils.c
@@ -6,15 +6,15 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 10:38:45 by meferraz          #+#    #+#             */
-/*   Updated: 2025/04/05 12:51:36 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/04/10 17:35:48 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-static int ft_rgb_to_hex(int r, int g, int b);
+static int	ft_rgb_to_hex(int r, int g, int b);
 
-void ft_set_floor_n_ceiling(t_game *game)
+void	ft_set_floor_n_ceiling(t_game *game)
 {
 	t_rgb	f_col;
 
@@ -26,11 +26,17 @@ void ft_set_floor_n_ceiling(t_game *game)
 
 static int	ft_rgb_to_hex(int r, int g, int b)
 {
-	if (r < 0) r = 0;
-	if (r > 255) r = 255;
-	if (g < 0) g = 0;
-	if (g > 255) g = 255;
-	if (b < 0) b = 0;
-	if (b > 255) b = 255;
+	if (r < 0)
+		r = 0;
+	if (r > 255)
+		r = 255;
+	if (g < 0)
+		g = 0;
+	if (g > 255)
+		g = 255;
+	if (b < 0)
+		b = 0;
+	if (b > 255)
+		b = 255;
 	return ((r << 16) | (g << 8) | b);
 }
