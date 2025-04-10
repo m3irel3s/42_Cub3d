23f67d73228a2dee4869ecc43f85beb9a75afc2c
Melanie Reis <melaniereis@Mac.Home>
2025-04-10 22:52:35 +0100

    add comments and fix norminette

--- src/600_rendering/render.c
+++ src/600_rendering/render.c
@@ -6,11 +6,27 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 14:20:00 by meferraz          #+#    #+#             */
-/*   Updated: 2025/04/10 21:12:11 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/04/10 22:52:03 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
+
+/**
+ * Darkens an RGB color by applying a factor multiple times.
+ *
+ * This function extracts the red, green, and blue components from
+ * a given color and repeatedly scales them by a specified factor.
+ * The operation is performed a given number of times for progressive
+ * darkening. If the factor is outside the range [0, 1], the original
+ * color is returned unchanged.
+ *
+ * @param color The original RGB color represented as an integer.
+ * @param factor The factor by which to darken the color components.
+ *               This should be a value between 0 and 1.
+ * @param times The number of times the darkening factor should be applied.
+ * @return The resulting darkened RGB color represented as an integer.
+ */
 
 int	ft_darken_rgb_color3(int color, double factor, int times)
 {
@@ -35,6 +51,18 @@ int	ft_darken_rgb_color3(int color, double factor, int times)
 	return ((r << 16) | (g << 8) | b);
 }
 
+/**
+ * Clears the top half of the screen with a gradient of the given ceiling color.
+ *
+ * This function uses ft_darken_rgb_color3 to darken the ceiling color by a
+ * factor of 0.9 a specified number of times, depending on the y-coordinate.
+ * The top of the screen is rendered with the brightest color, and the bottom
+ * of the ceiling with the darkest. The resulting color is then used to fill
+ * each horizontal line of the top half of the screen.
+ *
+ * @param game The game data structure, which contains the mlx image and window.
+ * @param ceiling_color The original RGB ceiling color represented as an integer.
+ */
 static void	ft_clear_ceiling(t_game *game, int ceiling_color)
 {
 	int	x;
@@ -58,6 +86,18 @@ static void	ft_clear_ceiling(t_game *game, int ceiling_color)
 	}
 }
 
+/**
+ * Clears the bottom half of the screen with a gradient of the given floor color.
+ *
+ * This function uses ft_darken_rgb_color3 to darken the floor color by a factor
+ * of 0.9 a specified number of times, depending on the y-coordinate. The bottom
+ * of the screen is rendered with the brightest color, and the top of the floor
+ * with the darkest. The resulting color is then used to fill each horizontal
+ * line of the bottom half of the screen.
+ *
+ * @param game The game data structure, which contains the mlx image and window.
+ * @param floor_color The original RGB floor color represented as an integer.
+ */
 static void	ft_clear_floor(t_game *game, int floor_color)
 {
 	int	x;
@@ -84,12 +124,32 @@ static void	ft_clear_floor(t_game *game, int floor_color)
 	}
 }
 
+/**
+ * Clears the screen with a gradient of the given ceiling and floor colors.
+ *
+ * This function simply calls ft_clear_ceiling and ft_clear_floor to clear the
+ * top and bottom halves of the screen, respectively.
+ *
+ * @param game The game data structure, which contains the mlx image and window.
+ * @param ceiling_color The original RGB ceiling color represented as an integer.
+ * @param floor_color The original RGB floor color represented as an integer.
+ */
 void	ft_clear_image(t_game *game, int ceiling_color, int floor_color)
 {
 	ft_clear_ceiling(game, ceiling_color);
 	ft_clear_floor(game, floor_color);
 }
 
+/**
+ * Renders the next frame of the game.
+ *
+ * This function first destroys the current frame, then creates a new one. It
+ * then clears the screen with a gradient of the given ceiling and floor colors,
+ * renders the raycasted walls, and finally puts the new frame in the window.
+ *
+ * @param game The game data structure, which contains the mlx image and window.
+ * @return 0 if the function was successful, 1 otherwise.
+ */
 int	ft_render_next_frame(t_game *game)
 {
 	if (game->img->mlx_img)
