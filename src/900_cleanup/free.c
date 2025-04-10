6367d9688f6b8621d776a674135e8e56243da682
Melmel <melmel@melmel.Home>
2025-04-10 15:04:00 +0100

    fix leaks

--- src/900_cleanup/free.c
+++ src/900_cleanup/free.c
@@ -12,17 +12,17 @@
 
 #include "cub3d.h"
 
-// static void	ft_free_textures(t_game *game);
-// static void	ft_clean_graphics(t_game *game);
+static void	ft_free_textures(t_game *game);
+static void	ft_clean_graphics(t_game *game);
 
 void	ft_cleanup(t_game *game, char *msg, int fd)
 {
 	if (game)
 	{
 		ft_free_parse(game);
-		// ft_free_textures(game);
-		// if (game->mlx)
-		// ft_clean_graphics(game);
+		ft_free_textures(game);
+		if (game->mlx)
+			ft_clean_graphics(game);
 		ft_free(game->map);
 		ft_free(game->player);
 		ft_free(game);
@@ -31,29 +31,29 @@ void	ft_cleanup(t_game *game, char *msg, int fd)
 	exit(EXIT_SUCCESS);
 }
 
-// static void	ft_clean_graphics(t_game *game)
-// {
-// 	if (!game || !game->mlx)
-// 		return;
-// 	if (game->img)
-// 	{
-// 		if (game->img->mlx_img)
-// 			mlx_destroy_image(game->mlx, game->img->mlx_img);
-// 		ft_free(game->img);
-// 	}
-// 	if (game->win)
-// 		mlx_destroy_window(game->mlx, game->win);
-// 	ft_free(game->mlx);
-// }
+static void	ft_clean_graphics(t_game *game)
+{
+	if (!game || !game->mlx)
+		return;
+	if (game->img)
+	{
+		if (game->img->mlx_img)
+			mlx_destroy_image(game->mlx, game->img->mlx_img);
+		ft_free(game->img);
+	}
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
+}
 
-// static void	ft_free_textures(t_game *game)
-// {
-// 	int	i;
+static void	ft_free_textures(t_game *game)
+{
+	int	i;
 
-// 	i = -1;
-// 	while (++i < 6)
-// 	{
-// 		if (game->textures[i].mlx_img)
-// 			mlx_destroy_image(game->mlx, game->textures[i].mlx_img);
-// 	}
-// }
+	i = -1;
+	while (++i < 6)
+	{
+		if (game->textures[i].mlx_img)
+			mlx_destroy_image(game->mlx, game->textures[i].mlx_img);
+	}
+}
