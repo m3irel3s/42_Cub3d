17d52fe454c3e2aaee164ef1ea52369aceb1975a
m3irel3s <miguelmeireles025@gmail.com>
2025-04-15 15:37:35 +0100

    merge

--- src/300_raycasting/dda_raycasting.c
+++ src/300_raycasting/dda_raycasting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dda_raycasting.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
+/*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 22:13:53 by meferraz          #+#    #+#             */
-/*   Updated: 2025/04/10 21:53:11 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:36:34 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,12 @@ void	ft_perform_dda(t_game *game, t_ray *ray)
 			ray->step.side_dist_y += ray->step.delta_dist_y;
 			ray->map_y += ray->step.step_y;
 			ray->side = 1;
+		}
+		if (ray->map_y < 0 || ray->map_y >= game->map->height ||
+			ray->map_x < 0 || ray->map_x >= (int)ft_strlen(game->map->grid[ray->map_y]))
+		{
+			ray->hit = 1;
+			break;
 		}
 		if (game->map->grid[ray->map_y][ray->map_x] == '1')
 			ray->hit = 1;
