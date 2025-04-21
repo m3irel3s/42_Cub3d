5c4e5c611ac0c42e9ead7c0827157b45706dfc49
Melanie Reis <melaniereis@Mac.Home>
2025-04-21 15:45:06 +0100

    fix norminette

--- src/300_raycasting/draw_raycasting.c
+++ src/300_raycasting/draw_raycasting.c
@@ -6,20 +6,21 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 22:14:50 by meferraz          #+#    #+#             */
-/*   Updated: 2025/04/08 21:49:26 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/04/21 15:42:32 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
+#include <math.h>
 
-void	ft_calc_wall(t_game *game, t_ray *ray)
+static void	ft_calc_wall_dist(t_game *game, t_ray *ray)
 {
 	if (ray->side == 0)
-		ray->perp_wall_dist = (ray->map_x - game->player->pos_x +
-							(1 - ray->step.step_x) / 2) / ray->ray_dir_x;
+		ray->perp_wall_dist = (ray->map_x - game->player->pos_x
+				+ (1 - ray->step.step_x) / 2) / ray->ray_dir_x;
 	else
-		ray->perp_wall_dist = (ray->map_y - game->player->pos_y +
-							(1 - ray->step.step_y) / 2) / ray->ray_dir_y;
+		ray->perp_wall_dist = (ray->map_y - game->player->pos_y
+				+ (1 - ray->step.step_y) / 2) / ray->ray_dir_y;
 	ray->line_height = (int)(SCREEN_HEIGHT / ray->perp_wall_dist);
 	ray->draw_start = -ray->line_height / 2 + SCREEN_HEIGHT / 2;
 	if (ray->draw_start < 0)
@@ -27,10 +28,16 @@ void	ft_calc_wall(t_game *game, t_ray *ray)
 	ray->draw_end = ray->line_height / 2 + SCREEN_HEIGHT / 2;
 	if (ray->draw_end >= SCREEN_HEIGHT)
 		ray->draw_end = SCREEN_HEIGHT - 1;
+}
+
+static void	ft_calc_wall_x_side(t_game *game, t_ray *ray)
+{
 	if (ray->side == 0)
-		ray->wall_x = game->player->pos_y + ray->perp_wall_dist * ray->ray_dir_y;
+		ray->wall_x = game->player->pos_y
+			+ ray->perp_wall_dist * ray->ray_dir_y;
 	else
-		ray->wall_x = game->player->pos_x + ray->perp_wall_dist * ray->ray_dir_x;
+		ray->wall_x = game->player->pos_x
+			+ ray->perp_wall_dist * ray->ray_dir_x;
 	ray->wall_x -= floor(ray->wall_x);
 	if (ray->side == 0)
 	{
@@ -48,3 +55,8 @@ void	ft_calc_wall(t_game *game, t_ray *ray)
 	}
 }
 
+void	ft_calc_wall(t_game *game, t_ray *ray)
+{
+	ft_calc_wall_dist(game, ray);
+	ft_calc_wall_x_side(game, ray);
+}
