626f0cafcc9b2ba44781d4cbf39cbe91f64a2351
Melanie Reis <melaniereis@Mac.Home>
2025-04-21 17:52:53 +0100

    improve header file

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -13,12 +13,15 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-// # include "../lib/minilibx-linux/mlx.h"
+// Include MiniLibX
 # include "../lib/minilibx/mlx.h"
+
+// Include custom libraries from 42 project
 # include "../42_Libft/FT_Libft/inc/libft.h"
 # include "../42_Libft/FT_Printf/inc/ft_printf.h"
 # include "../42_Libft/Get_next_line/inc/get_next_line.h"
 
+// Include standard libraries
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
@@ -27,9 +30,12 @@
 # include <sys/types.h>
 # include <sys/time.h>
 # include <math.h>
+
+// Include X11 for key handling
 # include <X11/X.h>
 # include <X11/keysym.h>
 
+// Include project-specific headers
 # include "macros.h"
 # include "types.h"
 # include "prototypes.h"
