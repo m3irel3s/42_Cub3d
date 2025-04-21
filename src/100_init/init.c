ba64a2138c16211e033b2e72f55d3749cfabd6dd
Melanie Reis <melaniereis@Mac.Home>
2025-04-21 21:37:53 +0100

    add comments and restructure init.c

--- src/100_init/init.c
+++ src/100_init/init.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 18:29:06 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/04/11 16:05:54 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/04/21 21:35:58 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
+static void		ft_init_textures(t_game *game);
 static t_map	*ft_init_map(void);
 
 t_game	*ft_init_structs(void)
 {
 	t_game	*game;
 
-	game = NULL;
 	game = ft_safe_malloc(sizeof(t_game));
 	if (!game)
 		return (NULL);
@@ -29,20 +29,24 @@ t_game	*ft_init_structs(void)
 	game->mlx = NULL;
 	game->win = NULL;
 	game->player = ft_safe_malloc(sizeof(t_player));
+	if (!game->player)
+		return (NULL);
+	ft_init_textures(game);
+	return (game);
+}
+
+static void	ft_init_textures(t_game *game)
+{
 	game->textures[0].mlx_img = NULL;
 	game->textures[1].mlx_img = NULL;
 	game->textures[2].mlx_img = NULL;
 	game->textures[3].mlx_img = NULL;
-	if (!game->player)
-		return (NULL);
-	return (game);
 }
 
 static t_map	*ft_init_map(void)
 {
 	t_map	*map;
 
-	map = NULL;
 	map = ft_safe_malloc(sizeof(t_map));
 	if (!map)
 		return (NULL);
